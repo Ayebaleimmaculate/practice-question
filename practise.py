6acc541708324e0f5ec914814f56b121d99deb17
Ayebaleimmaculate <142968605+Ayebaleimmaculate@users.noreply.github.com>
2023-10-06 01:13:59 -0700

    practise.py

---
+++ practise.py
@@ -0,0 +1,7 @@
+student_name= input() "Mary"
+print(student_name)
+class_section = "cohort3"
+print(class_section)
+#subject_mark
+html_mark=input("enter your mark scored")
+python_mark= input("enter your python mark scored")
